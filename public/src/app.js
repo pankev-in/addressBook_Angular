3677b4a8f91c1a2a6c35942910a8afa77b02e75a
Kevin Pan <pankevin128@gmail.com>
2015-10-17 19:33:05 +0200

    init, part1 Done + working on new contact form

---
+++ public/src/app.js
@@ -0,0 +1,13 @@
+angular.module('ContactsApp', ['ngRoute','ngResource'])
+	.config(function ($routeProvider, $locationProvider) {
+		$routeProvider
+			.when('/contacts', {
+				templateUrl: 'views/list.html',
+				controller: 'ListController'
+			})
+			.when('/contact/new', {
+				templateUrl: 'views/new.html',
+				controller: 'NewController'
+			});
+		$locationProvider.html5Mode(true);
+	});
